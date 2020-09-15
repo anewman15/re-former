cac7dd48d57e2c8ecba155de657e99de70fa36c9
Abdullah Numan <anewman15@hotmail.com>
2020-09-15 21:22:01 +0600

    Recreate user_new form using form_tag helper and methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new({username: params[:username], email: params[:email], password: params[:password]})
+    # @user = User.new(user_params)
+
     if @user.save
       redirect_to new_user_path
     else
