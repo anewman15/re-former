b3c91282e87383b4468ebfb4c7c7865c56384d70
Abdullah Numan <anewman15@hotmail.com>
2020-09-15 19:25:10 +0600

    Create html form, new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,15 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
 
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
