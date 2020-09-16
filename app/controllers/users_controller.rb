ee8fecd8e392abb2d1aed48a650d25fa76d2f173
Abdullah Numan <anewman15@hotmail.com>
2020-09-16 15:05:53 +0600

    Create edit form, update action, add error messagges to views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    render :edit
+  end
+
+  private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
